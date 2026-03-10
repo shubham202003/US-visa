b2c4392e00c01d115a48a68cfa3b31600e886a82
Shubham Maske <shubhammaske645@gmail.com>
2026-03-10 21:59:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shubham",
+    author_email="shubhammaske645@gmail.com",
+    packages=find_packages()
+)
